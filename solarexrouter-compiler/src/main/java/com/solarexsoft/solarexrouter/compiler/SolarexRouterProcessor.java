06aeb5731056868452d08f5d16de7d23e14fe17a
Solarex <rh.hou.work@gmail.com>
2020-03-03 22:48:35 +0800

    router processor

--- solarexrouter-compiler/src/main/java/com/solarexsoft/solarexrouter/compiler/SolarexRouterProcessor.java
+++ solarexrouter-compiler/src/main/java/com/solarexsoft/solarexrouter/compiler/SolarexRouterProcessor.java
@@ -1,4 +1,17 @@
 package com.solarexsoft.solarexrouter.compiler;
 
+import com.google.auto.service.AutoService;
+import com.solarexsoft.solarexrouter.compiler.utils.Constants;
+
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedOptions;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+
+@AutoService(Processor.class)
+@SupportedOptions(Constants.ARGUMENTS_NAME)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@SupportedAnnotationTypes({Constants.ANN_TYPE_ROUTE})
 public class SolarexRouterProcessor {
 }
