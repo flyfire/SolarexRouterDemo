5332c526d3eec05a68ab04ff51ed97cfc64803e2
Solarex <rh.hou.work@gmail.com>
2020-03-03 18:06:06 +0800

    route meta

---
+++ solarexrouter-annotation/src/main/java/com/solarexsoft/solarexrouter/annotation/model/RouteMeta.java
@@ -0,0 +1,52 @@
+package com.solarexsoft.solarexrouter.annotation.model;
+
+import javax.lang.model.element.Element;
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 18:01/2020/3/3
+ *    Desc:
+ * </pre>
+ */
+
+public class RouteMeta {
+    public enum JumpType {
+        ACTIVITY,
+        PROVIDER;
+    }
+
+    private JumpType jumpType;
+    private Element element;
+    private Class<?> destination;
+    private String path;
+    private String group;
+
+    public RouteMeta(JumpType jumpType, Element element, Class<?> destination, String path, String group) {
+        this.jumpType = jumpType;
+        this.element = element;
+        this.destination = destination;
+        this.path = path;
+        this.group = group;
+    }
+
+    public JumpType getJumpType() {
+        return jumpType;
+    }
+
+    public Element getElement() {
+        return element;
+    }
+
+    public Class<?> getDestination() {
+        return destination;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
