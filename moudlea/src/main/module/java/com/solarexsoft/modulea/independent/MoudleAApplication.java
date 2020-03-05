b6f4df40b6194a43461acbf12de0b4390edf38b6
Solarex <rh.hou.work@gmail.com>
2020-03-05 11:17:33 +0800

    demo

---
+++ moudlea/src/main/module/java/com/solarexsoft/modulea/independent/MoudleAApplication.java
@@ -0,0 +1,14 @@
+package com.solarexsoft.modulea.independent;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ *    Author: houruhou
+ *    CreatAt: 11:15/2020/3/5
+ *    Desc:
+ * </pre>
+ */
+
+public class MoudleAApplication extends Application {
+}
