d3378092a325be091dc7c8ae6c4ae34a11329e08
Solarex <rh.hou.work@gmail.com>
2020-03-05 10:54:33 +0800

    demo

--- moudlea/src/main/java/com/solarexsoft/moudlea/ModuleAActivity.java
+++ moudlea/src/main/java/com/solarexsoft/moudlea/ModuleAActivity.java
@@ -1,6 +1,15 @@
 package com.solarexsoft.moudlea;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.solarexsoft.base.ComponentShareProvider;
+import com.solarexsoft.solarexrouter.annotation.SolarexRouter;
+import com.solarexsoft.solarexroutercore.SolarexRouterCore;
 
 /**
  * <pre>
@@ -9,6 +18,39 @@ import androidx.appcompat.app.AppCompatActivity;
  *    Desc:
  * </pre>
  */
+@SolarexRouter(path = "/modulea/demo")
+public class ModuleAActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class ModuleAActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulea_demo);
+        findViewById(R.id.btn_jump_main).setOnClickListener(this);
+        findViewById(R.id.btn_jump_moduleb).setOnClickListener(this);
+        findViewById(R.id.btn_call_main).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_jump_main) {
+            if (BuildConfig.isModuleA) {
+                SolarexRouterCore.getInstance().build("/main/mainpage").navigation();
+            } else {
+                Toast.makeText(this, "独立模式", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.btn_jump_moduleb) {
+            if (BuildConfig.isModuleA) {
+                SolarexRouterCore.getInstance().build("/moduleb/demo").navigation();
+            } else {
+                Toast.makeText(this, "独立模式", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.btn_call_main) {
+            if (BuildConfig.isModuleA) {
+                ((ComponentShareProvider)(SolarexRouterCore.getInstance().build("/main/mainprovider").navigation())).callRemote();
+            } else {
+                Toast.makeText(this, "独立模式", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
--- moudleb/src/main/java/com/solarexsoft/moudleb/ModuleBActivity.java
+++ moudleb/src/main/java/com/solarexsoft/moudleb/ModuleBActivity.java
@@ -1,6 +1,14 @@
 package com.solarexsoft.moudleb;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.solarexsoft.solarexrouter.annotation.SolarexRouter;
+import com.solarexsoft.solarexroutercore.SolarexRouterCore;
 
 /**
  * <pre>
@@ -9,6 +17,31 @@ import androidx.appcompat.app.AppCompatActivity;
  *    Desc:
  * </pre>
  */
+@SolarexRouter(path = "/moduleb/demo")
+public class ModuleBActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moduleb_demo);
+        findViewById(R.id.btn_jump_main).setOnClickListener(this);
+        findViewById(R.id.btn_jump_modulea).setOnClickListener(this);
+    }
 
-public class ModuleBActivity extends AppCompatActivity {
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_jump_main) {
+            if (BuildConfig.isModuleB) {
+                SolarexRouterCore.getInstance().build("/main/mainpage").navigation();
+            } else {
+                Toast.makeText(this, "独立模式", Toast.LENGTH_SHORT).show();
+            }
+        } else if (id == R.id.btn_jump_modulea) {
+            if (BuildConfig.isModuleB) {
+                SolarexRouterCore.getInstance().build("/modulea/demo").navigation();
+            } else {
+                Toast.makeText(this, "独立模式", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
